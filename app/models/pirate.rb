d9334d1849c02ff89735559a1374734799716893
Sharmina Akhtar <sakht318@gmail.com>
2018-03-10 21:08:51 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def initialize(str)
+    @name = str[:name]
+    @weight = str[:weight]
+    @height = str[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  @@all = []
+
+  def initialize (str)
+    @name = str[:name]
+    @type = str[:type]
+    @booty = str[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def Ship.clear
+    @@all = []
+  end
 end
