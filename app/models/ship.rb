26575eafe33a2710762c7b07fdf892e17a2a30a5
Sharmina Akhtar <sakht318@gmail.com>
2018-03-10 20:19:20 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initialize (str)
+    @name = str[:name]
+    @type = str[:type]
+    @booty = str[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def Ship.clear
+    Ship.destroy
+  end
 end
